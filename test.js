9bcb6f84adf397e7a58a850ee13c822472b249f9
pojith <rpojith@gmail.com>
2024-07-31 20:36:15 +0530

    displaying

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const t=require('./SUM');
-  
+const lp=require('./wrk')
 test("is sucessfully:",()=>{ expect(t("jenfier","law")).toBe("jenfierlaw")})
-test("is sucessfully:",()=>{ expect(t("keer","thana")).toBe("keerthana")})
+test("is sucessfully:",()=>{ expect(lp()).toBe("heloo")})
 test("is sucessfully:",()=>{ expect(t("dhar","shni")).toBe("dharshni")})
