c6829f4e32cf3a62933d1e6e1fa7a8a3e56f34a7
pojith <rpojith@gmail.com>
2024-07-31 16:04:47 +0530

    chaged test case

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const t=require('./SUM');
   
-test("is sucessfully:",()=>{ expect(t(2,3)).toBe(5)})
-test("is sucessfully:",()=>{ expect(t(7,3)).toBe(10)})
-test("is sucessfully:",()=>{ expect(t(9,80)).toBe(89)})
+test("is sucessfully:",()=>{ expect(t("jenfier","law")).toBe("jenfierlaw")})
+test("is sucessfully:",()=>{ expect(t("keer","thana")).toBe("keerthana")})
+test("is sucessfully:",()=>{ expect(t("dhar","shni")).toBe("dharshni")})
